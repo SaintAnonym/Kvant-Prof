dd217ff1c1b2f639642e1fec73b0e5915b688eb8
UserNames <vladtlilun@gmail.com>
2022-03-09 06:51:34 +0500

    close vul

--- db/schema.rb
+++ db/schema.rb
@@ -12,11 +12,11 @@
 
 ActiveRecord::Schema.define(version: 2022_01_25_094839) do
 
-  create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    create_table "articles", force: :cascade do |t|
+      t.string "title"
+      t.text "body"
+      t.datetime "created_at", precision: 6, null: false
+      t.datetime "updated_at", precision: 6, null: false
+    end
+  
   end
-
-end
